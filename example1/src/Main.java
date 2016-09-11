2933a179cf5645eccc4dc49beb72fbbb64916092
elma1 <elmazolaa@gmail.com>
2016-09-11 14:30:33 -0400

    first example and first gitignore file for java eclipse

---
+++ example1/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int x;// declare a variable
+		x = 5;
+		System.out.println("x is  = " + x);
+		double y = 8.0;
+		System.out.println("y + x = ");
+		System.out.println(y + x);
+		System.out.println(y / x);
+		System.out.println(y % x);
+		String firstName = "Azar";
+		String lastName ="Hasanpour";
+		String fullName = firstName + " " +lastName;
+		System.out.println(fullName);
+		System.out.println(x < y);
+		boolean result = (y <= x);
+		System.out.println(result);
+		if(x > y){
+			System.out.println(" it is bigger");
+		}else{
+			System.out.println("it is not bigger");
+		}
+		
+		
+
+	}
+
+}
---
+++ example1/src/looping.java
@@ -0,0 +1,17 @@
+
+public class looping {
+	public static void main(String[] args){
+		for(int i = 0; i < 10; i++){
+			System.out.println("Azar");
+		}
+		for( int i = 0; i < 10; i++){
+			System.out.println(i);
+		}
+		int i = 5;
+		while(i < 10){
+			System.out.println("Happy");
+			i++;
+		}
+	}
+
+}
